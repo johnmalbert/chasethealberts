17db84a9c47c3386f3d16ab0de9485868bdf9208
John Albert <john.m.albert@gmail.com>
2024-12-16 19:46:10 -0800

    adding hikes

---
+++ src/components/hikesData.js
@@ -0,0 +1,44 @@
+// hikesData.js
+
+export const hikes = [
+  {
+    id: 1,
+    name: "Mount Daniel",
+    wildernessArea: "Alpine Lakes Wilderness",
+    length: 20, // miles
+    dogsAllowed: true,
+    highlights: "Class 2 and 3 scramble",
+    permit: "Self-permit",
+    distanceFromSeattle: 110, // miles
+    coverPhotos: ["/images/daniel.jpg"],
+    photos: ["/images/daniel.jpg", "/images/photo7.jpg"],
+    youtubeVideo: "https://www.youtube.com/embed/C3Y3m85lJ3k?si=ukDIFoH3W_3lHwAk",
+  },
+  {
+    id: 2,
+    name: "Mount Baker",
+    wildernessArea: "North Cascades",
+    length: 14, // miles
+    dogsAllowed: true,
+    highlights: "Roped In Glacier Travel - expertise required",
+    permit: false,
+    distanceFromSeattle: 120, // miles
+    coverPhotos: ["/images/photo13.jpg"],
+    photos: ["/images/photo13.jpg", "images/photo14.jpg"],
+    youtubeVideo: "https://www.youtube.com/embed/tN_t_plahUs?si=WKmjcE1GonhS7FdY",
+  },
+  {
+    id: 3,
+    name: "Glacier Peak",
+    wildernessArea: "Glacier Peak",
+    length: 30, // miles
+    dogsAllowed: true,
+    highlights: "Roped In Glacier Travel or Class 3 Scrambling - expertise required",
+    permit: "Self-Permit",
+    distanceFromSeattle: 100, // miles
+    coverPhotos: ["/images/glacier3.jpg"],
+    photos: ["/images/glacier.jpg", "/images/glacier2.jpg"],
+    youtubeVideo: "https://www.youtube.com/embed/RL_lw9CEsFQ?si=nEf6xDJ2qmnxr2P8",
+  },
+  // More hikes
+];
--- src/pages/TakeAHike.js
+++ src/pages/TakeAHike.js
@@ -1,48 +1,14 @@
 import React, { useState } from "react";
+import { hikes } from "../components/hikesData"; // Import the hikes data from hikesData.js
 import "../TakeAHike.css"; // Import styles for the page
 
 const TakeAHike = () => {
   const [selectedFilters, setSelectedFilters] = useState({
     distance: null,
     length: null,
-    wildernessArea: "", // Initially empty for the dropdown
+    wildernessArea: "",
   });
   const [selectedHike, setSelectedHike] = useState(null);
-
-  const hikes = [
-    {
-      id: 1,
-      name: "Mount Daniel",
-      wildernessArea: "Alpine Lakes Wilderness",
-      length: 20, // miles
-      dogsAllowed: true,
-      highlights: "Class 2 and 3 scramble",
-      permit: "Self-permit",
-      distanceFromSeattle: 110, // miles
-      photos: ["/images/daniel.jpg", "/images/photo7.jpg"],
-      youtubeVideo:
-        "https://www.youtube.com/embed/C3Y3m85lJ3k?si=ukDIFoH3W_3lHwAk",
-    },
-    {
-      id: 2,
-      name: "Mount Baker",
-      wildernessArea: "North Cascades",
-      length: 14, // miles
-      dogsAllowed: true,
-      highlights: "Roped In Glacier Travel - expertise required",
-      permit: false,
-      distanceFromSeattle: 120, // miles
-      photos: ["/images/photo13.jpg", "/images/photo14.jpg"],
-      youtubeVideo:
-        "https://www.youtube.com/embed/tN_t_plahUs?si=WKmjcE1GonhS7FdY",
-    },
-    // More hikes
-  ];
-
-  // Get unique wilderness areas for the dropdown
-  const wildernessAreas = [
-    ...new Set(hikes.map((hike) => hike.wildernessArea)),
-  ];
 
   // Filter hikes based on selected filters
   const filteredHikes = hikes.filter((hike) => {
@@ -57,6 +23,10 @@ const TakeAHike = () => {
     );
   });
 
+  // Get unique wilderness areas for the dropdown
+  const wildernessAreas = [
+    ...new Set(hikes.map((hike) => hike.wildernessArea)),
+  ];
   const handleHikeClick = (hike) => {
     setSelectedHike(hike);
   };
@@ -66,11 +36,12 @@ const TakeAHike = () => {
       <h1>Take a Hike</h1>
       <div className="centered-intro">
         <b>
-            <i>Looking for your next adventure? Here are some hikes we have enjoyed!
-                Use the filters below to narrow down your search.
-            </i>
+          <i>
+            Looking for your next adventure? Here are some hikes we have
+            enjoyed! Use the filters below to narrow down your search.
+          </i>
         </b>
-    </div>
+      </div>
       {/* Filters */}
       <div className="filters">
         <div className="filter">
@@ -127,7 +98,11 @@ const TakeAHike = () => {
             key={hike.id}
             onClick={() => handleHikeClick(hike)}
           >
-            <img src={hike.photos[0]} alt={hike.name} className="hike-photo" />
+            <img
+              src={hike.coverPhotos[0]}
+              alt={hike.name}
+              className="hike-photo"
+            />
             <div className="photo-title">{hike.name}</div>
           </div>
         ))}
@@ -176,10 +151,10 @@ const TakeAHike = () => {
               height="315"
               src={selectedHike.youtubeVideo}
               title="YouTube video player"
-              frameBorder="0"
+              frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
             ></iframe>
           )}
         </div>
