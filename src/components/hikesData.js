5e20fa786db34b1b6894703e10f28715f8f4d924
John Albert <john.m.albert@gmail.com>
2024-12-17 18:14:56 -0800

    adding four hikes

--- src/components/hikesData.js
+++ src/components/hikesData.js
@@ -10,8 +10,8 @@ export const hikes = [
     highlights: "Class 2 and 3 scramble",
     permit: "Self-permit",
     distanceFromSeattle: 110, // miles
-    coverPhotos: ["/images/daniel.jpg"],
-    photos: ["/images/daniel.jpg", "/images/photo7.jpg"],
+    coverPhotos: ["/images/photo7.jpg"],
+    photos: ["/images/hikes/daniel4.jpg", "images/hikes/daniel3.jpg"],
     youtubeVideo: "https://www.youtube.com/embed/C3Y3m85lJ3k?si=ukDIFoH3W_3lHwAk",
   },
   {
@@ -23,8 +23,8 @@ export const hikes = [
     highlights: "Roped In Glacier Travel - expertise required",
     permit: false,
     distanceFromSeattle: 120, // miles
-    coverPhotos: ["/images/photo13.jpg"],
-    photos: ["/images/photo13.jpg", "images/photo14.jpg"],
+    coverPhotos: ["/images/hikes/baker3.jpg"],
+    photos: ["/images/hikes/baker1.jpg", "images/hikes/baker2.jpg", "images/hikes/baker3.jpg", "images/hikes/baker4.jpg", "images/hikes/baker5.jpg", "images/hikes/baker6.jpg"],
     youtubeVideo: "https://www.youtube.com/embed/tN_t_plahUs?si=WKmjcE1GonhS7FdY",
   },
   {
@@ -37,8 +37,59 @@ export const hikes = [
     permit: "Self-Permit",
     distanceFromSeattle: 100, // miles
     coverPhotos: ["/images/glacier3.jpg"],
-    photos: ["/images/glacier.jpg", "/images/glacier2.jpg"],
+    photos: ["/images/hikes/glacierpeak1.jpg", "/images/hikes/glacierpeak2.jpg", "/images/hikes/glacierpeak3.jpg", "/images/hikes/glacierpeak4.jpg","/images/hikes/glacierpeak5.jpg"],
     youtubeVideo: "https://www.youtube.com/embed/RL_lw9CEsFQ?si=nEf6xDJ2qmnxr2P8",
   },
-  // More hikes
+  {
+    id: 4,
+    name: "Chelan Lakeshore Trail",
+    wildernessArea: "Entiat Mountains",
+    length: 20, // miles
+    dogsAllowed: true,
+    highlights: "Take a ferry to hike along this remote lakeshore",
+    permit: "Ferry Reservation",
+    distanceFromSeattle: 200, // miles
+    coverPhotos: ["/images/hikes/chelanlakeshore1.jpg"],
+    photos: ["/images/hikes/chelanlakeshore1.jpg","/images/hikes/chelanlakeshore3.jpg","/images/hikes/chelanlakeshore2.jpg"],
+    youtubeVideo: "https://www.youtube.com/embed/W2VVr-54P-U?si=8ScMIWgN9rXjQTzu",
+  },
+  {
+    id: 5,
+    name: "Golden Ears",
+    wildernessArea: "Golden Ears",
+    length: 20, // miles
+    dogsAllowed: true,
+    highlights: "Cross over into Canada for this delightful summit, and sleep on top of platforms above the lakes below",
+    permit: "True",
+    distanceFromSeattle: 180, // miles
+    coverPhotos: ["/images/hikes/goldenears3.jpg"],
+    photos: ["/images/hikes/goldenears1.jpg", "/images/hikes/goldenears2.jpg", "/images/hikes/goldenears3.jpg"],
+    youtubeVideo: "https://www.youtube.com/embed/kjVNye2uVUs?si=W57Ic-5rBwI5_GM8",
+  },
+  {
+    id: 6,
+    name: "Abernathy Peak",
+    wildernessArea: "Methow/Sawtooth",
+    length: 12, // miles
+    dogsAllowed: true,
+    highlights: "Scramble up this scree covered class three peak, and camp and the peaceful Scatter Lake with the mountain goats",
+    permit: "None",
+    distanceFromSeattle: 250, // miles
+    coverPhotos: ["/images/hikes/abernathy1.jpg"],
+    photos: ["/images/hikes/abernathy1.jpg", "/images/hikes/abernathy2.jpg"],
+    youtubeVideo: "https://www.youtube.com/embed/mDMWxQ8SSHc?si=f4N1i347DKnywDhP",
+  },
+  // {
+  //   id: ,
+  //   name: "",
+  //   wildernessArea: "",
+  //   length: , // miles
+  //   dogsAllowed: true,
+  //   highlights: "",
+  //   permit: "",
+  //   distanceFromSeattle: , // miles
+  //   coverPhotos: ["/images/hikes/"],
+  //   photos: ["/images/hikes/glacierpeak1.jpg"],
+  //   youtubeVideo: "",
+  // },
 ];
